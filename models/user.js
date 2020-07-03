5e8ff367bde3800a6ebd5a00844db883afce36e0
saisatwik99 <56591458+saisatwik99@users.noreply.github.com>
2020-07-03 22:22:17 +0530

    added user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var UserSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    stocks: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Watchlist"
+        }
+    ]
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User",UserSchema);
